be90dc2fbabc311c0c84b9b9fca15cfac995d9eb
Ferguson Shuai <ferguson_shuai@hotmail.com>
2021-10-15 15:48:55 -0700

    adding start script and set up environment variables

--- app.js
+++ app.js
@@ -114,6 +114,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 })
 
-app.listen(3000, () => {
-    console.log("App is listening on port 3000.")
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App is up and running on port ${port}.`)
 });
